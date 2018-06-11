a5d2f577671aeb655fe05c30a5c0ac56a9d7c245
MohammedTL <altemerabecs92@gmail.com>
2018-06-11 22:58:41 +0530

    SumClass->Change 2 ->From Team Leader

---
+++ GitProject/src/com/nit/test/SumClass.java
@@ -0,0 +1,10 @@
+package com.nit.test;
+
+public class SumClass {
+public void sum() {
+	for(int i=1;i<10;i++) {
+		System.out.println(i);
+	}
+	
+}
+}
--- GitProject/src/com/nit/test/TestClass.java
+++ GitProject/src/com/nit/test/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
 public static void main(String[] args) {
 	System.out.println("Hello Every body this is a test message for all you guys");
 	//Thank you , This is my attendance
+	new SumClass().sum();
 }
 }
